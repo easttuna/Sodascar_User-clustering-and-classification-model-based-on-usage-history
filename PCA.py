f95a12bc739f434cafadba4e1e222a79359ac45c
dmswl0707 <c950707@gmail.com>
2022-01-06 03:11:00 +0900

    update

--- PCA.py
+++ PCA.py
@@ -3,12 +3,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 import os
 
 
 data_dir = '/home/eunji/project_dir/sodescar/data/member.csv'
 
-# PCA
+### PCA
 
 # 총 컬럼 22개
 df = pd.read_csv(data_dir)
@@ -33,7 +34,6 @@ pca_array = pca.fit_transform(df_scaled)
 principal_df = pd.DataFrame(data=pca_array, columns=['component' + str(i) for i in range(1,19)])
 
 # scree plot
-
 plt.title('Scree plot')
 plt.xlabel('n_components')
 plt.ylabel('Cumulative Explained Variance')
@@ -44,8 +44,32 @@ pca_pred = pd.DataFrame(pca.fit_transform(df_x))
 
 # get predict value
 pca_pred = pd.concat([pca_pred, df_y], axis=1)
+
+# 2차원 시각화
 #sns.scatterplot(pca_pred[0], pca_pred[1], data=pca_pred, hue ='car_type_mode', style='car_type_mode', s=100)
 
+# 3차원 시각화
+'''
+pca_pred.columns=['com1','com2','com3','labels']
+fig2 = plt.figure(figsize=(10,10))
+ax = fig2.add_subplot(111, projection='3d')
+
+ax.set_xlabel('Pricipal Component1', fontsize=15)
+ax.set_ylabel('Pricipal Component2', fontsize=15)
+ax.set_zlabel('Pricipal Component3', fontsize=15)
+ax.set_title('3 Component PCA', fontsize=20)
+
+colors = ["#7fc97f","#beaed4","#fdc086","#ffff99","#386cb0","#f0027f"]
+labels = pca_pred['labels']
+
+for label, color in zip(labels, colors):
+    indicesToKeep = pca_pred['labels'] == label
+    ax.scatter(pca_pred.loc[indicesToKeep, 'com1'],
+                pca_pred.loc[indicesToKeep, 'com2'],pca_pred.loc[indicesToKeep, 'com3'], c = color, s = 10)
+
+ax.legend(labels)
+ax.grid()
+'''
 
 save_dir = './data'
 pca_pred.to_csv(os.path.join(save_dir, 'pca_pred.csv'), index=False)
